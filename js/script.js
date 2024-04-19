718c92091d5a1a5e103d2ae597b6e428625a1da5
Bedagya-Joshi <5h4d0mf0rc3@gmail.com>
2024-04-19 19:03:47 +0545

    redesign

--- js/script.js
+++ js/script.js
@@ -12,6 +12,45 @@ function handleScroll() {
     });
 }
 
+function openWhatsApp() {
+    const number = "9779844131555";
+    const url = `https://wa.me/${number}`;
+    window.open(url, "_blank");
+}
+
+function openSocialMedia(platform) {
+    const urls = {
+    facebook: "https://www.facebook.com/",
+    linkedin: "https://www.linkedin.com/",
+    github: "https://github.com/",
+    };
+    const url = urls[platform]; 
+    if (url) {
+    window.open(url, "_blank"); 
+    } else {
+    console.log("Invalid URL");
+    }
+}
+
+function downloadCV() {
+    const cvURL = "/Assets/Resume.pdf";
+    
+    const downloadLink = document.createElement("a");
+    downloadLink.href = cvURL;
+    downloadLink.download = "bedagya.pdf";
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+}
+
+const skillBars = document.querySelectorAll(".skill-bar");
+
+skillBars.forEach((skillBar) => {
+    const skillLevel = skillBar.dataset.skillLevel;
+    skillBar.style.width = `${skillLevel}%`;
+});
+
 window.addEventListener('scroll', handleScroll);
 
 handleScroll();
